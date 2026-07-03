94f19a87dc8813430a065a9d122a287e807b2c8b
Omoluabi1003 <pakiyogun10@gmail.com>
2026-07-03 06:31:56 -0400

    Point brand assets to repository logo source

--- lib/brand-assets.ts
+++ lib/brand-assets.ts
@@ -1,11 +1,8 @@
-export const campusitoryLogoJpegBase64 =
-  "REPLACE_ME";
-
-export const campusitoryLogoDataUri = `data:image/jpeg;base64,${campusitoryLogoJpegBase64}`;
-
 export const brandAssetConfig = {
   name: "Campusitory",
   tagline: "Knowledge in Motion",
   architect: "ETL GIS Consulting LLC",
   lead: "Paul A. K. Iyogun",
+  sourceLogo:
+    "https://raw.githubusercontent.com/Omoluabi1003/campusitory/main/1AFB3866-3262-4B5F-ABF4-15CA6B702952.png",
 } as const;
