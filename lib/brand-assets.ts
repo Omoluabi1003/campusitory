a6998cc8459303dca38fbce63e5c7ee3f0b29059
Omoluabi1003 <pakiyogun10@gmail.com>
2026-07-03 06:31:32 -0400

    Add Campusitory brand asset data holder

---
+++ lib/brand-assets.ts
@@ -0,0 +1,11 @@
+export const campusitoryLogoJpegBase64 =
+  "REPLACE_ME";
+
+export const campusitoryLogoDataUri = `data:image/jpeg;base64,${campusitoryLogoJpegBase64}`;
+
+export const brandAssetConfig = {
+  name: "Campusitory",
+  tagline: "Knowledge in Motion",
+  architect: "ETL GIS Consulting LLC",
+  lead: "Paul A. K. Iyogun",
+} as const;
